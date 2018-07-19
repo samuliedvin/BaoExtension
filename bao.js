7720de424408157ca1ba187c8aaafc3a1a9d861a
Samuli Virtanen <samuliedvin@gmail.com>
2018-07-19 19:24:17 +0300

    First commit

---
+++ bao.js
@@ -0,0 +1,47 @@
+const forEach = f => x => Array.prototype.forEach.call(x, f);
+
+
+
+function recursion(node) {
+        var textContent = "";
+        if(node.hasChildNodes()) {
+                childNodes = node.childNodes;
+                forEach((item) => {
+
+                        
+                        recursion(item);
+
+                })(childNodes);
+
+        } else {
+                textContent = node.textContent;
+				node.textContent = swapB(textContent);
+        }
+        
+}
+
+
+
+function swapB (text) {
+
+        for (var i = 0; i < text.length; i++) {
+                switch(text.charAt(i)) {
+                        case "p":
+                        case "P":
+                        case "B":
+                        case "b":	
+                                text = setCharAt(text, i, '🅱️');
+                                break;
+                        default:
+                                break;
+                }
+        }
+        return text;
+}
+
+function setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+recursion(document.body);
